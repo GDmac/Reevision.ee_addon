f6355d2773125d9a7619020416518a3b5f83db57
GDmac <grdalenoort@gmail.com>
2011-05-09 01:04:29 +0200

    smal fix and update

--- ft.reevision.php
+++ ft.reevision.php
@@ -8,7 +8,7 @@
  * @category	Fieldtypes
  * @author      GDmac
  * @copyright   Copyright (c) 2011 grdalenoort
- * @version     0.1
+ * @version     1.0.1
  * @license     http://creativecommons.org/licenses/by-sa/3.0/ Attribution-Share Alike 3.0 Unported
  */
 
@@ -16,7 +16,7 @@ class Reevision_ft extends EE_Fieldtype {
 
 	var $info = array(
 		'name'		=> 'Reevision (counter)',
-		'version'	=> '1.0'
+		'version'	=> '1.0.1'
 	);
 	var $has_array_data = FALSE;
 
@@ -30,7 +30,7 @@ class Reevision_ft extends EE_Fieldtype {
 	function display_field($data)
 	{
 		$this->EE->javascript->set_global('publish.hidden_fields', array($this->field_id => $this->field_name));
-		return "<div class=\"reevision\">$data</div" . form_hidden($this->field_name, $data+1);
+		return "<div class=\"reevision\">$data</div>" . form_hidden($this->field_name, $data+1);
 	}
 
 	// --------------------------------------------------------------------
@@ -42,7 +42,12 @@ class Reevision_ft extends EE_Fieldtype {
 	// --------------------------------------------------------------------
 	function display_settings($data)
 	{
-		$this->EE->table->add_row( 'Reevision', 'a simple counter which increments every time an entry is saved. Useful for instance for iCalendar where SEQUENCE:X denotes the revision. The field just outputs how many times the entry was saved. You can hide the field from the layout if you want. For safecracker set div.reevision to display:none;' );
+		$this->EE->table->add_row( 'Reevision', 'a simple counter which increments every time an entry is saved. 
+		Useful for instance for iCalendar where SEQUENCE:X denotes the revision. 
+		The field just outputs how many times the entry was saved. 
+		
+		You can hide the field from the layout if you want. 
+		For safecracker set in CSS: .reevision { display:none; }' );
 	}
 
 
